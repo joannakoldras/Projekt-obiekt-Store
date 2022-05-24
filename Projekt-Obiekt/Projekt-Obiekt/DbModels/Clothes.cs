f10b2862b6c232f0b66f46dde7e8cb1bdc742972
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-05-24 11:08:47 +0200

    add Brand, Category, Clothes, Color models

---
+++ Projekt-Obiekt/Projekt-Obiekt/DbModels/Clothes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt_Obiekt.DbModelds
+{
+    public class Clothes
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public virtual Category Id_Category { get; set; }
+        public virtual Brand Id_Brand { get; set; }
+        public virtual Color Id_Color { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Projekt-Obiekt/Projekt-Obiekt/DbModels/Color.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt_Obiekt.DbModelds
+{
+    public class Color
+    {
+        public int Id { get; set; }
+        public string ColorName { get; set; }
+    }
+}
