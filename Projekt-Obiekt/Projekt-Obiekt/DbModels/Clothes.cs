fc4d5e01407422eee30e9b2ce98e2471a092a953
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-05-24 11:20:34 +0200

    add annotations for Primary Key - autogenerated, required , foreign keys, and other constraints

--- Projekt-Obiekt/Projekt-Obiekt/DbModels/Clothes.cs
+++ Projekt-Obiekt/Projekt-Obiekt/DbModels/Clothes.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Projekt_Obiekt.DbModelds
 {
+    [Table("ClothesStore")]
     public class Clothes
     {
+        [Column("Id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         public int Id { get; set; }
         public int CategoryId { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
+
+        [ForeignKey("CategoryId")]
         public virtual Category Id_Category { get; set; }
+
+        [ForeignKey("BrandId")]
         public virtual Brand Id_Brand { get; set; }
+
+        [ForeignKey("ColorId")]
         public virtual Color Id_Color { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
+        [Required]
         public int Quantity { get; set; }
     }
 }
