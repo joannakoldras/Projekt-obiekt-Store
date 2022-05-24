aca3ef799567b2eee000366fba54b69b0285e584
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-05-24 11:19:44 +0200

    add annotations for Primary Key - autogenerated, required  and limitation for categoryName

--- Projekt-Obiekt/Projekt-Obiekt/DbModels/Category.cs
+++ Projekt-Obiekt/Projekt-Obiekt/DbModels/Category.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Projekt_Obiekt.DbModelds
 {
+    [Table("Categories")]
     public class Category
     {
+        [Column("CategoryId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string CategoryName { get; set; }
     }
 }
