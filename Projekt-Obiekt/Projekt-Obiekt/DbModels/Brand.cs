0557a0e63ecc06ace7bec7e4638ea8346c265b1c
Joanna Koldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-05-24 11:19:26 +0200

    add annotations for Primary Key - autogenerated, required  and limitation for brandName

--- Projekt-Obiekt/Projekt-Obiekt/DbModels/Brand.cs
+++ Projekt-Obiekt/Projekt-Obiekt/DbModels/Brand.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Projekt_Obiekt.DbModelds
 {
+    [Table("Brands")]
     public class Brand
     {
+        [Column("BrandId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string BrandName { get; set; }
     }
 }
